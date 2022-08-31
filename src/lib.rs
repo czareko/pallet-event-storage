136c01c1a2b8bcd188338093903e61c722c6710e
Cezary Olborski <cezary.olborski@gmail.com>
2022-08-31 11:24:37 +0800

    Cleaning - comments, dependencies, and info logs

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,8 @@ mod benchmarking;
 #[frame_support::pallet]
 pub mod pallet {
 	use chrono::{Duration, Utc};
-	use frame_support::{pallet_prelude::*, sp_runtime::traits::Hash};
+	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
-//	use sp_runtime::print;
-//	use sp_runtime::runtime_logger::RuntimeLogger;
-
 
 	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo)]
 	#[scale_info(skip_type_params(T))]
@@ -35,6 +32,13 @@ pub mod pallet {
 	#[pallet::without_storage_info]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
+
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+
+	}
+
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -83,27 +87,6 @@ pub mod pallet {
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 
-/*
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn check_custom_struct(origin: OriginFor<T>) -> DispatchResult {
-
-			let message = "event message".to_string();
-
-			//let bt = Bytes::from(vec![1,2]);
-
-			//let event = CustomEvent{value: bt, time_stamp: Utc::now()};
-
-			//println!("CustomEvent (value length): {}",event.value.to_vec().len());
-			//println!("CustomEvent (time): {}",event.time_stamp);
-
-			// Emit an event.
-			let who = ensure_signed(origin)?;
-			let something = 42;
-			<Something<T>>::put(something);
-			//<Something<T>>::
-			Self::deposit_event(Event::EventStored(something, who));
-			Ok(())
-		}*/
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
@@ -146,16 +129,11 @@ pub mod pallet {
 			let who = ensure_signed(origin)?;
 			let content_value = message;
 			let tstamp = Utc::now().timestamp_nanos();
-			//let tstamp_key = T::Hashing::hash_of(&tstamp);
 			let custom_event = CustomEvent { time_stamp: tstamp, content: content_value.clone(), reporter: who.clone()};
 
 			<CustomEvents<T>>::insert(tstamp,custom_event);
 
 			Self::deposit_event(Event::EventStored(tstamp,who.clone()));
-
-			// print("ABC:");
-			// debug::RuntimeLogger::init();
-			// debug::info!("DEBUG INFO");
 
 			Ok(())
 		}
@@ -167,7 +145,6 @@ pub mod pallet {
 			let mut to_remove =vec![];
 			let mut removed_elems=0;
 
-			//let storage_snapshot = <CustomEvents<T>>::
 			/*
 			If the storage will be altered during the iteration we can have undefined results
 			To limit this problem, we split this process into two parts.
@@ -175,7 +152,6 @@ pub mod pallet {
 			let keys = <CustomEvents<T>>::iter_keys();
 			for key in keys{
 				let tnow = (Utc::now()-Duration::seconds(1)).timestamp_nanos();
-				//let value = <CustomEvents<T>>::get(key).unwrap();
 				if key<tnow {
 					to_remove.push(key);
 				}
